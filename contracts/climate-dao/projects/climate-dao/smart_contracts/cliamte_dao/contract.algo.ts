0ac16f4676aef3907bdb0b51d6de5408809d2083
Aneesh <aneeshsharmag@gmail.com>
2025-07-03 22:39:48 +0530

    deploy config

--- contracts/climate-dao/projects/climate-dao/smart_contracts/cliamte_dao/contract.algo.ts
+++ contracts/climate-dao/projects/climate-dao/smart_contracts/cliamte_dao/contract.algo.ts
@@ -1,4 +1,4 @@
-import { abi } from '@algorandfoundation/algokit-utils';
+import { abi } from "@algorandfoundation/algokit-utils";
 
 // --- Decorator placeholders ---
 function GlobalState(_type: any) {
--- contracts/climate-dao/projects/climate-dao/smart_contracts/cliamte_dao/deploy-config.ts
+++ contracts/climate-dao/projects/climate-dao/smart_contracts/cliamte_dao/deploy-config.ts
@@ -1,33 +1,12 @@
-import { AlgorandClient } from '@algorandfoundation/algokit-utils'
-import { CliamteDaoFactory } from '../artifacts/cliamte_dao/CliamteDaoClient'
+import { AppDeploymentConfig } from "@algorandfoundation/algokit-utils/types";
 
-// Below is a showcase of various deployment options you can use in TypeScript Client
-export async function deploy() {
-  console.log('=== Deploying CliamteDao ===')
+const deployConfig: AppDeploymentConfig = {
+  appName: "climate_dao",
+  creator: {
+    address: "REPLACE_WITH_YOUR_TESTNET_ADDRESS"
+  },
+  deployTimeParams: {},
+  deleteIfExists: true,
+};
 
-  const algorand = AlgorandClient.fromEnvironment()
-  const deployer = await algorand.account.fromEnvironment('DEPLOYER')
-
-  const factory = algorand.client.getTypedAppFactory(CliamteDaoFactory, {
-    defaultSender: deployer.addr,
-  }) as ReturnType<typeof algorand.client.getTypedAppFactory>
-
-  const { appClient, result } = await factory.deploy({ onUpdate: 'append', onSchemaBreak: 'append' })
-
-  // If app was just created fund the app account
-  if (['create', 'replace'].includes(result.operationPerformed)) {
-    await algorand.send.payment({
-      amount: (1).algo(),
-      sender: deployer.addr,
-      receiver: appClient.appAddress,
-    })
-  }
-
-  const method = 'hello'  
-  const response = await appClient.send.hello({
-    args: { name: 'world' },
-  })
-  console.log(
-    `Called ${method} on ${appClient.appClient.appName} (${appClient.appClient.appId}) with name = world, received: ${response.return}`,
-  )
-}
+export default deployConfig;
