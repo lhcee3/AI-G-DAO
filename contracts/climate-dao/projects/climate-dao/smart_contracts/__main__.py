6792957d7c5f55e09acc225dc9a5de4bd819bd67
Aneesh <aneeshsharmag@gmail.com>
2025-07-14 02:10:32 +0530

    changes
     1. New main.py for scs  2. Documenation 4th page

--- contracts/climate-dao/projects/climate-dao/smart_contracts/__main__.py
+++ contracts/climate-dao/projects/climate-dao/smart_contracts/__main__.py
@@ -1,4 +1,8 @@
-from algorand_python import cli
+from smart_contracts.contract import approval_program, clear_program
 
 if __name__ == "__main__":
-    cli()
+    with open("approval.teal", "w") as f:
+        f.write(approval_program().teal())
+
+    with open("clear.teal", "w") as f:
+        f.write(clear_program().teal())
