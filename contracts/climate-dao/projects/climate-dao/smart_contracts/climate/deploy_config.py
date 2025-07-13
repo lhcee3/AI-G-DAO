f9d28641567318b1373a8da9afc26f228182e7ed
Aneesh <aneeshsharmag@gmail.com>
2025-07-14 02:13:56 +0530

    revising the structure once aggain

--- contracts/climate-dao/projects/climate-dao/smart_contracts/climate/deploy_config.py
+++ contracts/climate-dao/projects/climate-dao/smart_contracts/climate/deploy_config.py
@@ -1,6 +1,44 @@
-from algokit_utils.config import DeploymentConfig
-from dataclasses import dataclass
+import logging
 
-@dataclass
-class ClimateDAOConfig(DeploymentConfig):
-    pass  # Optional config can be added here
+import algokit_utils
+
+logger = logging.getLogger(__name__)
+
+
+# define deployment behaviour based on supplied app spec
+def deploy() -> None:
+    from smart_contracts.artifacts.climate.climate_client import (
+        HelloArgs,
+        ClimateFactory,
+    )
+
+    algorand = algokit_utils.AlgorandClient.from_environment()
+    deployer_ = algorand.account.from_environment("DEPLOYER")
+
+    factory = algorand.client.get_typed_app_factory(
+        ClimateFactory, default_sender=deployer_.address
+    )
+
+    app_client, result = factory.deploy(
+        on_update=algokit_utils.OnUpdate.AppendApp,
+        on_schema_break=algokit_utils.OnSchemaBreak.AppendApp,
+    )
+
+    if result.operation_performed in [
+        algokit_utils.OperationPerformed.Create,
+        algokit_utils.OperationPerformed.Replace,
+    ]:
+        algorand.send.payment(
+            algokit_utils.PaymentParams(
+                amount=algokit_utils.AlgoAmount(algo=1),
+                sender=deployer_.address,
+                receiver=app_client.app_address,
+            )
+        )
+
+    name = "world"
+    response = app_client.send.hello(args=HelloArgs(name=name))
+    logger.info(
+        f"Called hello on {app_client.app_name} ({app_client.app_id}) "
+        f"with name={name}, received: {response.abi_return}"
+    )
