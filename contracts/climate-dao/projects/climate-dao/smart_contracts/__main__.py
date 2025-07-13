d5f67660cfd934dc3071b02601771417f16a4b0b
Aneesh <aneeshsharmag@gmail.com>
2025-07-14 02:38:22 +0530

    build status - 0

--- contracts/climate-dao/projects/climate-dao/smart_contracts/__main__.py
+++ contracts/climate-dao/projects/climate-dao/smart_contracts/__main__.py
@@ -1,7 +1,5 @@
-# main.py
-
 from pyteal import compileTeal, Mode
-from contract import approval_program, clear_state_program
+from climate.contract import approval_program, clear_state_program  
 
 if __name__ == "__main__":
     with open("approval.teal", "w") as f:
