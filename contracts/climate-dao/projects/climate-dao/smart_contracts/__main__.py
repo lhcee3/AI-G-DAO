b1deb0bb5de15e30402cf4cdd3ded65a3e60e273
Aneesh <aneeshsharmag@gmail.com>
2025-07-21 12:31:52 +0530

    Del: Removed Second Life Cycle
    of smart contracts

--- contracts/climate-dao/projects/climate-dao/smart_contracts/__main__.py
+++
@@ -1,9 +0,0 @@
-from pyteal import compileTeal, Mode
-from .climate.contract import approval_program, clear_state_program
-
-if __name__ == "__main__":
-    with open("approval.teal", "w") as f:
-        f.write(compileTeal(approval_program(), mode=Mode.Application, version=8))
-
-    with open("clear.teal", "w") as f:
-        f.write(compileTeal(clear_state_program(), mode=Mode.Application, version=8))
--- contracts/climate-dao/projects/climate-dao/smart_contracts/climate/deploy_config.py
+++
@@ -1,6 +0,0 @@
-from algokit_utils.config import DeploymentConfig
-from dataclasses import dataclass
-
-@dataclass
-class ClimateDAOConfig(DeploymentConfig):
-    pass  # Optional config can be added here
